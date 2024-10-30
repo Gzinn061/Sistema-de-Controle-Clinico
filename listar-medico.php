fbdb84121ac620f81b81b0da61cdf6f5e11a395a
Gzinn061 <martinsnoguilherme@gmail.com>
2024-10-30 17:02:43 -0300

    Códigos atualizados

--- listar-medico.php
+++ listar-medico.php
@@ -1,30 +1,39 @@
 <h1>Listar Médico</h1>
 <?php
-	$sql = "SELECT * FROM medico";
+$sql = "SELECT * FROM medico";
 
-	$res = $conn ->query($sql);
+$res = $conn->query($sql);
 
-	$qtd = $res->num_rows;
+$qtd = $res->num_rows;
 
 
-	if ($qtd > 0) {
-		print "<p>Encontrou <b>$qtd</b> resultado(s)</p>";
-		print "<table class='table table-bordered table-striped table-hover'>";
+if ($qtd > 0) {
+	print "<p>Encontrou <b>$qtd</b> resultado(s)</p>";
+	print "<table class='table table-bordered table-striped table-hover'>";
+	print "<tr>";
+	print "<th>#</th>";
+	print "<th>Nome</th>";
+	print "<th>CRM</th>";
+	print "<th>Especialidade</th>";
+	print "</tr>";
+	while ($row = $res->fetch_object()) {
 		print "<tr>";
-		print "<th>#</th>";
-		print "<th>Nome</th>";
-		print "<th>CRM</th>";
-		print "<th>Especialidade</th>";
+		print "<td>" . $row->id_medico . "</td>";
+		print "<td>" . $row->nome_medico . "</td>";
+		print "<td>" . $row->crm_medico . "</td>";
+		print "<td>" . $row->especialidade_medico . "</td>";
 		print "</tr>";
-		while  ($row = $res->fetch_object()) {
-			print "<tr>";
-			print "<td>".$row->id_medico."</td>";
-			print "<td>".$row->nome_medico."</td>";
-			print "<td>".$row->crm_medico."</td>";
-			print "<td>".$row->especialidade_medico."</td>";
-			print "</tr>";
-		}
-		print "</table>";
-	} else {
-		print "Não encontrou resultados!";
+		print "<td>
+			 	<button class='btn btn-success' 
+				onclick=\"
+				location.href='?page=editar-medico&id_medico="
+			. $row->id_medico . "';\">Editar</button>
+			 	
+				<button class ='btn btn-danger'onclick=\"if(confirm('Tem certeza que deseja excluir?')){location.href='?page=salvar-medico&acao=excluir&id_medico=" . $row->id_medico . "';}else{false;}\">Excluir</button>
+				</td>";
+		print "<tr>";
 	}
+	print "</table>";
+} else {
+	print "Não encontrou resultados!";
+}
--- salvar-medico.php
+++ salvar-medico.php
@@ -1,12 +1,12 @@
 
 <?php
-	switch (@$_REQUEST['acao']) {
-		case 'cadastrar':
-			$nome = $_REQUEST['nome_medico'];
-			$crm = $_REQUEST['crm_medico'];
-			$especialidade = $_REQUEST['especialidade_medico'];
+switch (@$_REQUEST['acao']) {
+	case 'cadastrar':
+		$nome = $_REQUEST['nome_medico'];
+		$crm = $_REQUEST['crm_medico'];
+		$especialidade = $_REQUEST['especialidade_medico'];
 
-			$sql = "INSERT INTO medico (
+		$sql = "INSERT INTO medico (
 							nome_medico,
 							crm_medico,
 							especialidade_medico
@@ -15,20 +15,34 @@
 						'{$crm}',
 						'{$especialidade}'
 					)";
-				$res = $conn->query($sql);
-			
+		$res = $conn->query($sql);
 
-				if ($res==true) {
-					print "<script>alert('Cadastrou com sucesso!');</script>";
-					print "<script>location.href='?page=listar-medico';</script>";
-				} else {
-					print "<script>alert('Deu errado');</script>";
-					print "<script>location.href='?page=listar-medico';</script>";
-				}
-				break;
-				case 'editar':
 
-				break;
-				case 'excluir':
-				
-	}
+		if ($res == true) {
+			print "<script>alert('Cadastrou com sucesso!');</script>";
+			print "<script>location.href='?page=listar-medico';</script>";
+		} else {
+			print "<script>alert('Deu errado');</script>";
+			print "<script>location.href='?page=listar-medico';</script>";
+		}
+		break;
+	case 'editar':
+		$nome = $_POST['nome_medico'];
+		$crm = $_POST['crm_medico'];
+		$especialidade = $_POST['especialidade_medico'];
+
+		break;
+	case 'excluir':
+		$sql = "DELETE FROM medico WHERE id_medico=" . $_REQUEST['id_medico'];
+
+		$res = $conn->query($sql);
+
+		if ($res == true) {
+			print "<script>alert('Excluiu com sucesso!');</script>";
+			print "<script>location.href='?page=listar-medico';</script>";
+		} else {
+			print "<script>alert('Deu errado');</script>";
+			print "<script>location.href='?page=listar-medico';</script>";
+		}
+		break;
+}
